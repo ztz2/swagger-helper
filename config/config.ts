f9cf1f0e95d79f4483efda84ba9f95d65becf9d0
448941273@qq.com <as80393313hj>
2022-05-31 19:00:58 +0800

    cli: 打包优化

--- config/config.ts
+++ config/config.ts
@@ -2,17 +2,79 @@ import { defineConfig } from 'umi';
 const { NODE_ENV } = process.env;
 import routes from './routes';
 
+const isProduction = NODE_ENV === 'production';
+
 export default defineConfig({
+  title: 'Swagger Helper',
   nodeModulesTransform: {
     type: 'none',
   },
   routes,
   fastRefresh: {},
-  sass: {},
+  externals: {
+    lodash: '_',
+    'highlight.js': 'hljs',
+    'crypto-js': 'CryptoJS',
+    clipboard: 'ClipboardJS',
+    'art-template': 'template',
+
+    moment: 'moment',
+    react: 'React',
+    'react-dom': 'ReactDOM',
+    antd: 'antd',
+  },
+  links: [
+    {
+      rel: 'stylesheet',
+      href: 'https://lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/antd/4.18.9/antd.min.css',
+    },
+    {
+      rel: 'stylesheet',
+      href: 'https://lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/highlight.js/11.4.0/styles/vs2015.min.css',
+    },
+  ],
+  scripts: [
+    {
+      src: 'https://lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/lodash.js/4.17.21/lodash.min.js',
+    },
+    {
+      src: 'https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-M/art-template/4.13.2/lib/template-web.min.js',
+    },
+    {
+      src: 'https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-M/highlight.js/11.4.0/highlight.min.js',
+    },
+    {
+      src: 'https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-M/crypto-js/4.1.1/crypto-js.min.js',
+    },
+    {
+      src: 'https://lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/clipboard.js/2.0.10/clipboard.min.js',
+    },
+    {
+      src: 'https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-M/moment.js/2.29.1/moment.min.js',
+    },
+    isProduction
+      ? {
+          src: 'https://lf26-cdn-tos.bytecdntp.com/cdn/expire-1-M/react/17.0.2/umd/react.production.min.js',
+        }
+      : {
+          src: 'https://lf26-cdn-tos.bytecdntp.com/cdn/expire-1-M/react/17.0.2/umd/react.development.min.js',
+        },
+    isProduction
+      ? {
+          src: 'https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-M/react-dom/17.0.2/umd/react-dom.production.min.js',
+        }
+      : {
+          src: 'https://lf3-cdn-tos.bytecdntp.com/cdn/expire-1-M/react-dom/17.0.2/umd/react-dom.development.min.js',
+        },
+    {
+      src: 'https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-M/antd/4.18.9/antd.min.js',
+    },
+  ],
+
   proxy: {
     '/api': {
-      target: 'https://swaggerhelper.andou.live:9527',
-      // target: 'http://127.0.0.1:9528',
+      // target: 'https://swaggerhelper.andou.live:9527',
+      target: 'http://127.0.0.1:9528',
       // changeOrigin: true,
       // pathRewrite: {
       //   '^/api': ''
@@ -20,7 +82,7 @@ export default defineConfig({
     },
   },
   chainWebpack(config, { env, webpack, createCSSRule }) {
-    if (NODE_ENV === 'production'){
+    if (isProduction) {
       // config.merge({
       //   optimization: {
       //     splitChunks: {
