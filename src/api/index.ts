477b3f1078f99da060547a7757a4a90e7a154a2a
448941273@qq.com <as80393313hj>
2022-06-03 18:01:39 +0800

    fix: 兼容新版

--- src/api/index.ts
+++ src/api/index.ts
@@ -6,15 +6,8 @@ export const getSwagger = (data: any) => {
   if (data.password) {
     data.password = aes.encrypt(data.password, SECRET_KEY);
   }
-  return request('/api/swagger-docs', {
+  return request('/app/swagger-helper/api/swagger-docs', {
     method: 'post',
-    data
-  });
-};
-
-export const importSwagger = (data: any) => {
-  return request('/api/import-swagger', {
-    method: 'post',
-    data
+    data,
   });
 };
--- src/utils/request.ts
+++ src/utils/request.ts
@@ -3,28 +3,32 @@ import { message } from 'antd';
 
 const request = extend({
   dataField: '',
-  parseResponse: false
+  parseResponse: false,
 });
 
 request.interceptors.response.use(
   async (response: any) => {
     const body = await response.json();
-    if (body?.code === 200) {
+    if (body?.code === 1000) {
       return body.data;
     }
-    message.error(body.msg);
-    return Promise.reject(body.msg);
+    message.error(body.message);
+    return Promise.reject(body.message);
   }, // @ts-ignore
   (error: any) => {
     const status = error?.response?.status;
-    const text = error.response.msg ?? error.response.message ?? error.response.statusText ?? '出错了';
+    const text =
+      error.response.message ??
+      error.response.message ??
+      error.response.statusText ??
+      '出错了';
     if (status === 401) {
       message.error('没有权限访问该项目');
     } else {
       message.error(text);
     }
     return Promise.reject(error);
-  }
+  },
 );
 
 export default request;
