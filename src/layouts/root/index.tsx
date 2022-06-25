000ff0dcabb22e46a3de5a869c27db95bf37d3a2
448941273@qq.com <as80393313hj>
2022-06-25 14:27:54 +0800

    refactor:

--- src/layouts/root/index.tsx
+++ src/layouts/root/index.tsx
@@ -1,4 +1,6 @@
 import { useEffect } from 'react';
+import { Space } from 'antd';
+import { GithubOutlined } from '@ant-design/icons';
 import { connect, getDvaApp, IRouteComponentProps } from 'umi';
 import { PersistGate } from 'redux-persist/integration/react';
 import styles from './index.scss';
@@ -24,6 +26,23 @@ function Layout({
   return (
     // @ts-ignore
     <PersistGate loading={null} persistor={persistor}>
+      <div className={styles.appHeader}>
+        <div className={styles.appHeader__l}>
+          <a href="/" className={styles.appLog}>
+            SWAGGER HELPER
+          </a>
+        </div>
+        <div className={styles.appHeader__r}>
+          <Space size={20}>
+            <a href="https://ztz-table.andou.live:9527" target="_blank">
+              ZTZ TABLE 组件
+            </a>
+            {/*<a href="https://github.com/ztz2/ztz-table" target="_blank">*/}
+            {/*    <GithubOutlined style={{'font-size': '28px', 'color': '#333333'}} />*/}
+            {/*</a>*/}
+          </Space>
+        </div>
+      </div>
       <div className={styles.container}>{children}</div>
       <Report />
     </PersistGate>
